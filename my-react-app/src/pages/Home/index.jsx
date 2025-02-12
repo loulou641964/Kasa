a38b2607f2736999ea24e93ea6b1ff1c802e3cb5
guy coste <guycoste86@gmail.com>
2025-02-12 17:45:08 +0100

    modification de la structure de base ajout des pakages

--- my-react-app/src/pages/Home/index.jsx
+++ my-react-app/src/pages/Home/index.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import reactLogo from './../../assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
-
+import './../../App.css'
+import data from './../../data/logements.json'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -16,7 +16,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>{data[0].title}</h1>
+      <img src={data[0].cover} alt="" />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
