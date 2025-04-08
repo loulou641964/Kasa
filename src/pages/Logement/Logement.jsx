fc3c37a2a4cd825da88976af649ab3634ec0ce22
guy coste <guycoste86@gmail.com>
2025-04-08 17:41:24 +0200

    commit

--- src/pages/Logement/Logement.jsx
+++ src/pages/Logement/Logement.jsx
@@ -1,11 +1,23 @@
 import { useParams } from "react-router-dom";
+import logements from "../../data/logements.json";
 
-function Logement (){ 
-    const params =useParams()
-    console.log(params.id)
+function Logement() { 
+    const params = useParams();
+    const logement = logements.find(item => item.id === params.id);
+
+    if (!logement) {
+        console.error(`Logement with id ${params.id} not found.`);
+        return <p>Logement introuvable</p>;
+    }
+
+    console.log(logement); 
+    
     return ( 
-        <p>page logement</p>
-
-    )
+        <div>
+            <h1>{logement.title}</h1>
+            <p>{logement.location}</p>
+        </div>
+    );
 }
+
 export default Logement;
